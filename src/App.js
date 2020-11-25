0e76633c9969cb7b52a9d3c85cfd01861bffb2f8
Mark9Mbugua <mbuguamark@gmail.com>
2020-11-25 21:25:09 +0300

    created basic UI structure using react-router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import Header from './components/header/header.component';
+import Footer from './components/footer/footer.component';
+
+import Home from  './pages/home/home.component';
+import Education from './pages/education/education.component';
+import Projects from './pages/projects/projects.component';
+import ContactMe from './pages/contact-me/contact-me.component';
+
 function App() {
   return (
     <div>
-      <h1>Mark Mbugua's Portfolio</h1>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/education" component={Education} />
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/contact-me" component={ContactMe} />
+      </Switch>
+      <Footer />
     </div>
   );
 }
---
+++ src/pages/education/education.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Education = () => {
+    return (
+        <div>
+            <h1>My Education Page</h1>
+        </div>
+    )
+}
+
+export default Education
