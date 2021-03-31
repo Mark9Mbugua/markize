977a3dbd51dad3ff9ad05dba8ac971350108d39b
Mark9Mbugua <mbuguamark@gmail.com>
2021-03-31 13:26:34 +0300

    Created all the footer features

--- src/components/footer/footer.styles.jsx
+++ src/components/footer/footer.styles.jsx
@@ -1,25 +1,53 @@
 import styled from 'styled-components';
 
 export const FooterContainer = styled.div`
-    min-height: 10%;
+    min-height: 20%;
     bottom: 0;
     background: inherit;
     display: flex;
     justify-content: center;
     align-items: center;
     width: 100%;
+    /* border: 1px solid red; */
 `;
 
 
 export const FooterBody = styled.div`
-    width: 33%; 
+    width: 75%; 
     padding-bottom: 10px;
     display: flex;
-    justify-content: center;
-    align-items: center; 
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    /* border: 1px solid white;  */
+`;
+
+export const FooterIcons = styled.div`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: 20%; 
+    /* border: 1px solid white; */
+    margin-bottom: 50px; 
+`;
+
+export const FooterInfoContainer = styled.div`
+    text-align: center;
 `;
 
 export const FooterInfo = styled.p`
     font-size: 20px;
-    font-family: 'Dancing Script', cursive;
+    color:#9396a3;
+    letter-spacing: 3px;
+`;
+
+
+export const SocialIconSrc = styled.img`
+    height: 30px;
+    width: 30px;
+    cursor: pointer;
+
+    &:hover {
+        color:#ff6161;
+    }
 `;
