89b565eaf0b3bbcd5abadf8f21ba64c70a437e41
Mark9Mbugua <mbuguamark@gmail.com>
2021-04-21 00:38:56 +0300

    Added GSAP animations on Contact Section

--- src/components/contact/contact.component.jsx
+++ src/components/contact/contact.component.jsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 import CustomButton from '../custom-button/custom-button.component';
 
@@ -12,10 +15,38 @@ import {
     EmailLink 
 } from './contact.styles';
 
+gsap.registerPlugin(ScrollTrigger);
+
 const Contact = () => {
+    let contactSection = useRef(null);
+    let contactBody = useRef(null);
+
+    useEffect(() =>{
+        const contactBodyFirst = contactBody.children[0];
+        const contactBodySecond = contactBodyFirst.nextSibling;
+        const contactBodyThird = contactBodySecond.nextSibling;
+        const contactBodyFourth = contactBodyThird.nextSibling;
+
+        let tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: contactSection,
+                start: 'top 50%'
+
+            }
+        });
+
+        tl
+        .from(contactBodyFirst, {opacity: 0, x: -200, duration: .4, ease: 'Power3.easeInOut', stagger: .8})
+        .from([contactBodySecond, contactBodyThird, contactBodyFourth],
+            {opacity: 0, y: 100, duration: .8, ease: 'Power3.easeInOut', stagger: .6});
+    }, []);
+
     return (
-        <ContactSection id='contact'>
-            <ContactBody>
+        <ContactSection 
+            id='contact'
+            ref={el => contactSection = el}
+        >
+            <ContactBody ref={el => contactBody = el}>
                 <div>
                     <WhatNext>What's Next?</WhatNext>
                 </div>
